36706d54240d45332c158b0c7570f3eca7044459
Miles Jaffee <mej327@lehigh.edu>
2025-05-18 19:30:18 -0400

    beautified page, added loading counter

--- app/page.jsx
+++ app/page.jsx
@@ -12,17 +12,39 @@ const DynamicMap = dynamic(() => import('../components/Map'), {
 export default function Home() {
   const [locations, setLocations] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [numBooks, setNumBooks] = useState(0);
+  const [loadedBooks, setLoadedBooks] = useState(0);
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+  const interval = setInterval(() => {
+    if (loading) {
+      sleep(333);
+      setLoadedBooks((prev) => {
+        if (prev + 1 >= numBooks) {
+          clearInterval(interval);
+          return numBooks;
+        }
+        return prev + 1;
+      });
+    }
+  }, 100);
 
   async function handleUpload(e) {
-    const file = e.target.files[0];
+    const file = e.target.files?.[0];
     if (!file) return;
 
     setLoading(true);
 
     Papa.parse(file, {
       header: true,
+      skipEmptyLines: true,
       complete: async (results) => {
         const csvData = results.data;
+
+        setNumBooks(csvData.length);
 
         const response = await fetch('/api/process-csv', {
           method: 'POST',
@@ -39,12 +61,20 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center p-4">
-      <h1 className="text-2xl mb-4">Upload your Goodreads CSV</h1>
-      <input type="file" accept=".csv" onChange={handleUpload} className="mb-4" />
-      {loading && <p>Processing...</p>}
+      <h1 className="text-6xl mb-4">Author Mapper</h1>
+      <p className="text-lg mb-4">Upload a Goodreads CSV and see a map of all your favorite authors' birthplaces.</p>
+      <p className="text-lg mb-4">Note: This may take a while to process, depending on the number of authors.</p>
+      <p className="text-lg mb-4">Get your CSV file here: <a className="italic underline" href="https://www.goodreads.com/review/import">My Books - Import/Export</a></p>
+      <input id="csvInput" type="file" accept=".csv" onChange={handleUpload} className="mb-4 underline" />
+      {loading && <p>Processing... [{loadedBooks}/{numBooks}]</p>}
       {!loading && locations.length > 0 && (
         <DynamicMap locations={locations}/>
       )}
+      <div id="footer" className="mt-4">
+        <p className="text-sm">
+
+        </p>
+      </div>
     </div>
   );
 }
