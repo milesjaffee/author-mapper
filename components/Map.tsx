b0404b4b117286b6e58f48b812f4818b78b02d5e
Miles Jaffee <mej327@lehigh.edu>
2025-05-18 14:53:12 -0400

    fixed markers looking weird

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLngExpression } from 'leaflet';
+import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 interface Location {
@@ -12,12 +12,23 @@ interface Location {
   interface MapProps {
     locations: Location[];
   }
+
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+      'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl:
+      'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl:
+      'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  });
   
   export default function DynamicMap({ locations }: MapProps) {
-    const center: LatLngExpression = [20, 0]; // Explicitly type the center
+    const center: L.LatLngExpression = [20, 0]; // Explicitly type the center
   
     return (
-      <MapContainer center={center} zoom={2} style={{ height: '600px', width: '100%' }}>
+      <MapContainer center={center} zoom={2} style={{ height: '600px', width: '100%' }} attributionControl={true}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {locations.map((loc, idx) => (
           <Marker key={idx} position={[loc.lat, loc.lon]}>
