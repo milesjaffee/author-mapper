23ee50eb577f0ef1499034a44a9d82085655baca
Miles Jaffee <mej327@lehigh.edu>
2025-06-22 10:40:44 -0400

    added my linkedin to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ return (
       <h1 className="text-6xl mb-4">Author Mapper</h1>
 
       <p>
-          <strong>This project is not affiliated with Goodreads or Amazon.</strong> <a href="https://www.wikidata.org/wiki/Wikidata:Database_download" className="underline">Wikidata</a> is used to find authors' birthplaces, and <a href="https://leafletjs.com/" className="underline">Leaflet</a> is used to display the map. For any concerns or questions, please contact <a href="mailto:mej327@lehigh.edu" className="underline">mej327@lehigh.edu</a>.
+          <strong>This project is not affiliated with Goodreads or Amazon.</strong> <a href="https://www.wikidata.org/wiki/Wikidata:Database_download" className="underline">Wikidata</a> is used to find authors' birthplaces, and <a href="https://leafletjs.com/" className="underline">Leaflet</a> is used to display the map. For any concerns or questions, please contact <a href="mailto:mej327@lehigh.edu" className="underline">mej327@lehigh.edu</a>. You can also find me on Linkedin: <a href="https://www.linkedin.com/in/milesjaffee/" className="underline">Miles Jaffee</a>.
         </p>
 
         <p className="mt-4"><a href="/">Return Home</a></p>
